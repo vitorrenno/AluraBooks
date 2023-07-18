d633f2f87949dd9cda85ed49e9ade7901c08f94f
Vitor Renno <vitorrenno@outlook.com>
2023-07-18 10:26:45 -0300

    Curso finalizado

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,11 +1,15 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
         </h2>
@@ -17,3 +21,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
       </div>`
     })
   }
+
+//   function verificarDisponibilidadeDoLivro(livro) {
+//     if (livro.quantidade > 0) {
+//         return 'livro__imagens'
+//     } else {
+//         return 'livro__imagens indisponivel'
+//     }
+//   }
